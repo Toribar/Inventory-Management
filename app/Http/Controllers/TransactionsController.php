3f0631310c66c3189c13a5400499164964a2212d
Nemanja <nemanja.d.krstic@gmail.com>
2016-02-22 18:52:43 +0100

    checkpoint

---
+++ app/Http/Controllers/TransactionsController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Transaction;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TransactionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = Product::find($request->product_id);
+
+        $price = $product->sell_price;
+
+        $quantity = $request->quantity;
+
+        if ($request->direction == 'in') {
+            $price = $product->purchase_price;
+        } else {
+            $quantity = -$quantity;
+        }
+
+        Transaction::create([
+            'product_id' => $product->id,
+            'quantity' => $quantity,
+            'price' =>  $price,
+        ]);
+
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,4 +18,23 @@
 			</tr>
 		@endforeach
 	</table>
+
+	{{ $products->render() }}
+
+	<hr>
+
+	{{ Form::open(['route' => 'transactions.index']) }}
+		<div class="form-group">
+			<label>Artikl</label>
+			{{ Form::select('product_id', $products->pluck('name', 'id'), null, ['class' => 'form-control']) }}
+		</div>
+
+		<div class="form-group">
+			<label>Količina</label>
+			{{ Form::text('quantity', null, ['class' => 'form-control']) }}
+		</div>
+
+		<button type="submit" class="btn btn-primary" name="direction" value="in">Ulaz</button>
+		<button type="submit" class="btn btn-primary" name="direction" value="out">Izlaz</button>
+	{{ Form::close() }}
 @stop
