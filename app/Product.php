0a1bbfee460e2b2c4bbaca3e8a69e9b7429b249a
Nemanja <nemanja.d.krstic@gmail.com>
2016-02-22 18:33:18 +0100

    update quantity function

--- app/Product.php
+++ app/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     {
         return $this->hasMany(Transaction::class);
     }
+
+    public function getQuantity()
+    {
+        return $this->transactions()->sum('quantity');
+    }
 }
